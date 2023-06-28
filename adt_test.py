83c2ca4339760d063d28df60cec3bcb65895dd0f
Chad Harper <chad.harper@gmail.com>
2023-06-28 19:35:05 -0400

    In class 6/28
    Added the queue test; changed test prompt to use match

--- adt_test.py
+++ adt_test.py
@@ -1,7 +1,8 @@
 # Test routines for unit testing
-# abstract data types.
+# abstract data types. 
 
 from adt import Stack
+from adt import Queue
 
 def test_stack():
     s = Stack()
@@ -21,16 +22,41 @@ def test_stack():
     print(s.pop())
     print(s.peek())
 
+def test_queue():
+    q = Queue()
+    empty_queue = q.dequeue()
+    if empty_queue == None:
+        print("Empty queue test passed")
+    else:
+        print("Empty queue test failed")
+        exit(-1)
+    
+    for i in range(10):
+        q.enqueue(i)
+    
+    print(q.size())
+    for j in range(q.size()):
+        print(q.dequeue())
+
+
 def test_main():
     r = ''
     while r != 'x':
         print('ADT Test')
         print('Select an option below:')
         print('1. Test Stack')
+        print('2. Test Queue')
         print('x. Exit')
         r = input('Enter your choice: ')
-        if r.isnumeric() and int(r) == 1:
-            test_stack()
+        if r.isnumeric():
+            selection = int(r)
+            match selection:
+                case 1:
+                    test_stack()
+                case 2:
+                    test_queue()
+                case _:
+                    print("Not implemented")
 
 if __name__ == '__main__':
     test_main()
